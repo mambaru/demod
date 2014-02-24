4abc06af780bf9aab32d94aabb0bd61585d77c65
Vladimir Migashko <migashko@gmail.com>
2014-02-25 03:06:17 +0400

    upd

--- demod/demod.cpp
+++ demod/demod.cpp
@@ -5,7 +5,7 @@
 #include <logger/logger_module.hpp>
 #include <logger/logger_module.hpp>
 #include <hub/hub_module.hpp>
-#include <pubsub_service/pubsub_service_module.hpp>
+//#include <pubsub_service/pubsub_service_module.hpp>
 
 #include "build_info.h"
 #include <core/build_info.h>
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
       { "core",    std::make_shared< wfc::core_module   >() },
       { "config",  std::make_shared< wfc::config_module >() },
       { "logger",  std::make_shared< wfc::logger_module >() }, 
-      { "hub",     std::make_shared< wfc::hub_module    >() },
+      { "hub",     std::make_shared< wfc::hub_module    >() }//,
 //      { "pubsub-service",     std::make_shared< wfc::pubsub_service_module >() },
 //      { "demo",    std::make_shared< wamba::demo::demo_module >() }
     }
