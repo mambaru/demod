074e5eb751357054517f047f58b416107f2758b8
Vladimir Migashko <migashko@gmail.com>
2014-03-12 02:59:23 +0400

    upd

--- tests/tank/wfc.sh
+++ tests/tank/wfc.sh
@@ -1,4 +1,4 @@
-export LD_LIBRARY_PATH=/home/migashko/prj/phantom/lib
+export LD_LIBRARY_PATH=/home/migashko/prj/phantom/lib/pantom
 export PATH=$PATH:/home/migashko/prj/yandex-tank:/home/migashko/prj/phantom/bin:`pwd`
 echo $PATH
-tank.py -l /tmp/tank/load.log -o "tank.artifacts_base_dir=/tmp/tank/result" -o "tank.artifacts_dir=/tmp/tank/result" -o "tank.lock_dir=/tmp/tank/lock" $@
+tank.py -l /tmp/tank/load.log -o "tank.artifacts_base_dir=/tmp/tank/result" -o "tank.artifacts_dir=/tmp/tank/result" -o "tank.lock_dir=/tmp/tank/lock" -o "tank.phantom_modules_path=/home/migashko/prj/phantom/bin" $@
