3be5d6f7709794a10d6973f93b36b78a520b2b76
Vladimir Migashko <migashko@gmail.com>
2015-02-06 19:11:33 +0300

    core cleanup

--- demod/demod.cpp
+++ demod/demod.cpp
@@ -4,7 +4,7 @@
 #include <startup/startup_module.hpp>
 #include <config/config_module.hpp>
 #include <logger/logger_module.hpp>
-#include <logger/logger_module.hpp>
+#include <broker/broker_module.hpp>
 #include <hub/hub_module.hpp>
 #include <repli_hub/repli_hub_module.hpp>
 #include <pubsub_service/pubsub_service_module.hpp>
@@ -26,6 +26,7 @@ int main(int argc, char* argv[])
       { "config",  std::make_shared< wfc::config_module >() },
       { "logger",  std::make_shared< wfc::logger_module >() }, 
       { "hub",     std::make_shared< wfc::hub_module    >() },
+      { "broker",     std::make_shared< wfc::broker_module    >() },
       { "repli-hub",     std::make_shared< wfc::repli_hub_module >() },
       { "pubsub-service",     std::make_shared< wfc::pubsub_service_module >() },
       { "pubsub-gateway",     std::make_shared< wfc::pubsub_gateway_module >() }, 
