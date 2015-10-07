cf74b9fc566f8980057d09719a91474ba948d46b
Vladimir Migashko <migashko@gmail.com>
2015-10-07 20:26:08 +0300

    upd

--- demod/demod.cpp
+++ demod/demod.cpp
@@ -4,10 +4,17 @@
 #include <wfcroot/wfcroot.hpp>
 #include <wfc_demo/demo/demo_package.hpp>
 
+struct my_build_info: demod_build_info
+{
+  std::string version() const { return "1.0"; } 
+};
+
 int main(int argc, char* argv[])
 {
+  
+  
   return ::wfc::wfc(
-    ::wfc::make_build_info<demod_build_info>(), 
+    ::wfc::make_build_info<my_build_info>(), 
     ::wfc::wfcroot({
       std::make_shared< ::wamba::demo_package >()
     })
